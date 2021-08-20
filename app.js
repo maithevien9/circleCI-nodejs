c98656d15ddc622c342f99eaeb854a34ddac39a2
maithevien9 <maithevien9@gmail.com>
2021-08-20 15:47:41 +0700

    config

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(process.env.PORT || 5000,function () {
+  console.log('server is running ' + process.env.PORT || 5000);
+});
